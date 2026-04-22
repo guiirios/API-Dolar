4f7349a25b74e4a81a2c166a8b8090f22ae3f02d
Guilherme Rios <110192250+guiirios@users.noreply.github.com>
2026-04-22 11:59:17 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,39 @@
+#import api
+from flask import Flask 
+
+#buscar informação de sites
+#requests
+import requests
+
+#criando um api
+app = Flask(__name__)
+
+#liga uma função a alguma coisa
+#quando acessar essa rota ligue esta função0
+@app.route("/")
+def home():
+    #retornando um JSON com significado, ja que uma api é enviar
+    #informação e trazer cm singificado
+    return {"mensagem": "api funcionando"}
+
+
+
+
+#rota da api
+@app.route("/dolar")
+def dolar():
+    #buscar dados
+    url = requests.get("https://economia.awesomeapi.com.br/json/last/USD-BRL")
+    #estruturar os dados
+    dados = url.json()
+    #retornar
+    valor = float(dados["USDBRL"]["bid"])
+    ask = float(dados["USDBRL"]["ask"])
+
+    return {
+        "dolar bid" :f"R$ {round(valor, 2)}",
+         "dolar ask":f"R$ {round(ask, 2)}"
+        }
+
+app.run()
+
